aa4e27b27c1ea57f1e68729bedd79ddbff1e2207
Оксана <axushka2008@gmail.com>
2024-10-14 15:18:45 +0300

    add task-1, task-2, task-3.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,30 @@
 
 
-function slugify(title) {
-const slug = title.toLowerCase();
-return slug.replace(/\s+/g, '-');
+function isEnoughCapacity(products, containerSize) {
+   const totalProducts = Object.values(products);
+   let totalNumber = 0;
+   for(const item of totalProducts) {
+totalNumber += item;
+   }
+   if(totalNumber < containerSize) { return true;
+   } else {
+    return false
+   }
 
+console.log(totalProducts);
 }
-
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+  ); // false
+  
+  console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+  ); // false
